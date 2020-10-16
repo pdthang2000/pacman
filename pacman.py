b7178c9ba52418471bc411cc3196f09fedbd26df
pdthang <pdthang2000@gmail.com>
2020-10-16 14:52:50 +0700

    alo

---
+++ pacman.py
@@ -0,0 +1,214 @@
+import copy
+import pygame as pygame
+import math
+
+pygame.init()
+originalGameBoard = [
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3],
+    [3, 2, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 2, 3, 3, 2, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 2, 3],
+    [3, 6, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 2, 3, 3, 2, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 6, 3],
+    [3, 2, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 2, 3, 3, 2, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 2, 3],
+    [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3],
+    [3, 2, 3, 3, 3, 3, 2, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 2, 3, 3, 3, 3, 2, 3],
+    [3, 2, 3, 3, 3, 3, 2, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 2, 3, 3, 3, 3, 2, 3],
+    [3, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 2, 2, 3],
+    [3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 1, 3, 3, 1, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 1, 3, 3, 1, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 2, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 2, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 2, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 2, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 2, 3, 3, 1, 3, 4, 4, 4, 4, 4, 4, 3, 1, 3, 3, 2, 3, 3, 3, 3, 3, 3],
+    [1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 3, 4, 4, 4, 4, 4, 4, 3, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1],  # Middle Lane Row: 14
+    [3, 3, 3, 3, 3, 3, 2, 3, 3, 1, 3, 4, 4, 4, 4, 4, 4, 3, 1, 3, 3, 2, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 2, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 2, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 2, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 2, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 2, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 2, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 2, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 2, 3, 3, 3, 3, 3, 3],
+    [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3],
+    [3, 2, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 2, 3, 3, 2, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 2, 3],
+    [3, 2, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 2, 3, 3, 2, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 2, 3],
+    [3, 6, 2, 2, 3, 3, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 6, 3],
+    [3, 3, 3, 2, 3, 3, 2, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 2, 3, 3, 2, 3, 3, 3],
+    [3, 3, 3, 2, 3, 3, 2, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 2, 3, 3, 2, 3, 3, 3],
+    [3, 2, 2, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 3, 3, 2, 2, 2, 2, 2, 2, 3],
+    [3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3],
+    [3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3],
+    [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+]
+gameBoard = copy.deepcopy(originalGameBoard)
+spriteRatio = 3 / 2
+square = 15  # Size of each unit square
+spriteOffset = square * (1 - spriteRatio) * (1 / 2)
+(width, height) = (len(gameBoard[0]) * square, len(gameBoard) * square)  # Game screen
+print(spriteOffset)
+screen = pygame.display.set_mode((width, height))
+pygame.display.flip()
+game_over = False  # Biến đánh dấu sự kiện kết thúc game
+
+pos = [13, 26]
+pelletColor = (222, 161, 133)
+# print(len(gameBoard[0]))  # horizontal
+# print(len(gameBoard))  # vertical
+clock = pygame.time.Clock()
+src = "src/"
+YellowOrb = "yellow2.jpg"  # hình tròn vàng
+y = "image1.jpg"  # tường màu xanh
+
+
+def initPac():
+    p = pygame.image.load(src + YellowOrb)
+    p = pygame.transform.scale(p, (square, square))
+    screen.blit(p, (14 * square, 26 * square, square, square))
+
+
+class Pacman:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.pacSpeed = 1 / 4
+        self.dir = 0  # 0: North, 1: East, 2: South, 3: West 0 là đi lên. 1 là sang phải 2 là xuống dưới 3 là sang trái
+        self.newDir = 0
+
+    def update(self):
+        if self.newDir == 0:
+            if canMove(math.floor(self.row - self.pacSpeed), self.col) and self.col % 1.0 == 0:
+                self.row -= self.pacSpeed
+                self.dir = self.newDir
+                # print(0)
+                return
+        elif self.newDir == 1:  # 26 , 13,5
+            if canMove(self.row, math.ceil(self.col + self.pacSpeed)) and self.row % 1.0 == 0:
+                self.col += self.pacSpeed
+                self.dir = self.newDir
+                # print(1)
+                return
+        elif self.newDir == 2:
+            if canMove(math.ceil(self.row + self.pacSpeed), self.col) and self.col % 1.0 == 0:
+                self.row += self.pacSpeed
+                self.dir = self.newDir
+                # print(2)
+                return
+        elif self.newDir == 3:
+            if canMove(self.row, math.floor(self.col - self.pacSpeed)) and self.row % 1.0 == 0:
+                self.col -= self.pacSpeed
+                self.dir = self.newDir
+                # print(3)
+                return
+
+        if self.dir == 0:
+            if canMove(math.floor(self.row - self.pacSpeed), self.col) and self.col % 1.0 == 0:
+                self.row -= self.pacSpeed
+            # print(0)
+        elif self.dir == 1:
+            if canMove(self.row, math.ceil(self.col + self.pacSpeed)) and self.row % 1.0 == 0:
+                self.col += self.pacSpeed
+            # print(1)
+        elif self.dir == 2:
+            if canMove(math.ceil(self.row + self.pacSpeed), self.col) and self.col % 1.0 == 0:
+                self.row += self.pacSpeed
+            # print(2)
+        elif self.dir == 3:
+            if canMove(self.row, math.floor(self.col - self.pacSpeed)) and self.row % 1.0 == 0:
+                self.col -= self.pacSpeed
+            # print(3)
+
+    def clearCurrentPacmanImage(self):
+        if check(self.row, self.col):
+            self.drawBlackRectangle(math.floor(self.row), self.col)
+            self.drawBlackRectangle(math.ceil(self.row), self.col)
+            self.drawBlackRectangle(self.row, math.floor(self.col))
+            self.drawBlackRectangle(self.row, math.ceil(self.col))
+        else:
+            self.drawBlackRectangle(self.row, self.col)
+
+    def drawBlackRectangle(self, row, col):  # draw image
+        pygame.draw.rect(screen, (0, 0, 0), (col * square, row * square, square, square))
+
+    def clear(self):
+        row = math.floor(self.row)
+        col = math.floor(self.col)
+        print(row)
+        print(col)
+        print('\n')
+        for i in range(row - 2, row + 3):
+            for j in range(col - 2, col + 3):
+                if 3 <= i < len(gameBoard) - 2 and 0 <= j < len(gameBoard[0]):
+                    if gameBoard[i][j] == 2 or gameBoard[i][j] == 1:
+                        pygame.draw.rect(screen, (0, 0, 0),
+                                         (self.col * square, self.row * square, square, square))
+                        return
+                        # draw black rectangle override
+
+    # Draws pacman based on his current state
+
+    def drawCurrentPacmanImage(self):  # draw image
+        p = pygame.image.load(src + YellowOrb)
+        p = pygame.transform.scale(p, (square, square))
+        screen.blit(p, (self.col * square, self.row * square, square, square))
+
+
+def canMove(row, col):
+    # if col == -1 or col == len(gameBoard[0]):
+    #     return True
+    if gameBoard[int(row)][int(col)] != 3:
+        return True
+    return False
+
+
+def check(row, col):
+    if row != int(row):
+        return True
+    if col != int(col):
+        return True
+    return False
+
+
+def pause(time):
+    cur = 0
+    while not cur == time:
+        cur += 1
+
+
+def drawMap():
+    screen.fill((0, 0, 0))  # fill background màu black
+    for i in range(len(gameBoard)):
+        for j in range(len(gameBoard[0])):
+            if gameBoard[i][j] == 3:
+                tileImage = pygame.image.load(src + y)
+                tileImage = pygame.transform.scale(tileImage, (square, square))
+                screen.blit(tileImage, (j * square, i * square, square, square))
+            elif gameBoard[i][j] == 2:  # Draw Tic-Tak
+                pygame.draw.circle(screen, pelletColor, (j * square + square // 2, i * square + square // 2),
+                                   square // 4)
+
+
+drawMap()
+fps = 30
+fpsClock = pygame.time.Clock()
+P = Pacman(26, 14)
+initPac()
+pygame.display.update()
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                P.newDir = 0
+            elif event.key == pygame.K_d:
+                P.newDir = 1
+            elif event.key == pygame.K_s:
+                P.newDir = 2
+            elif event.key == pygame.K_a:
+                P.newDir = 3
+    P.clearCurrentPacmanImage()
+    P.update()
+    P.drawCurrentPacmanImage()  # draw Pacman
+    pygame.display.update()
+    fpsClock.tick(fps)
